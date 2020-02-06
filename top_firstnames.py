323402f618ad16643f3f0f50e5906eed8e8701b9
devimprov <40347090+devimprov@users.noreply.github.com>
2020-02-06 11:42:31 -0500

    add comments and accept arguments

--- top_firstnames.py
+++ top_firstnames.py
@@ -1,21 +1,31 @@
 import operator
 import json
+import sys 
 
-#Read Friends
-with open("friends.json","r") as file:
+if len(sys.argv) > 1:
+    friendsfile = sys.argv[1]
+else:
+    friendsfile = "friends.json"
+
+
+#Read friends.json file as json
+with open(friendsfile,"r") as file:
     friends = json.loads(file.read())
 
+#Extract list of friends names from file
 friendlist = []
 
 for friend in friends["friends"]:
     friendlist.append(friend['name'])
 
+#Extract first names from friends list
 firstnames = []
 
 for friend in friendlist:
     first = friend.split(" ")[0]
     firstnames.append(first)
 
+#Get how many of each name there are, store value in dict
 namecount = {}
 
 for name in firstnames:
@@ -26,8 +36,12 @@ for name in firstnames:
 
     namecount[name] = namecount[name] + 1
 
+#Sort the data so the largest amount of names is on top. This outputs as list of tuples with name and number
+
 sorted_namedata = sorted(namecount.items(), key = operator.itemgetter(1), reverse=True)
 
+#Loop through the list of tuples and print tha name and how many instances of the name there were
 for name in sorted_namedata:
     name, number = name
     print(f"{name} - {number}")
+
